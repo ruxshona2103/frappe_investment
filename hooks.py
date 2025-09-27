a1bf450913643154dff3a0afb51e500ea109ee25
Asadtop4ik <asadbek.backend@gmail.com>
2025-09-27 12:08:42 +0500

    error fixed

--- hooks.py
+++
@@ -1,6 +0,0 @@
-app_name = "investment"
-app_title = "Investment"
-app_publisher = "ruxshona2103"
-app_description = "Investment Management App"
-app_email = "your_email@example.com"
-app_license = "MIT"
---
+++ investment/hooks.py
@@ -0,0 +1,3 @@
+# Alias/proxy: Frappe 'investment.hooks' ni import qiladi,
+# biz esa haqiqiy hooks ni 'frappe_investment.hooks' dan olamiz.
+from frappe_investment.hooks import *
